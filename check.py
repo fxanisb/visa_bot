87dbe6e1f0e7ca29251353ab60ebe039437ead43
fxanisb <anisbarkat93@gmail.com>
2026-03-05 05:23:43 +0100

    check.py

--- check.py
+++ check.py
@@ -29,6 +29,7 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
+options.binary_location = "/usr/bin/google-chrome"
 
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 20)
@@ -114,4 +115,5 @@ while True:
         print("Error:", e)
 
         time.sleep(30)
+
 
